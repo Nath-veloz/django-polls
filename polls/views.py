32899eb3e61b420536685375bb37ff7095eba3b3
Nath-veloz <nathansluz24@gmail.com>
2023-05-03 09:21:28 -0300

    Urls polls

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# Nova view, leva para algum rota
+def index(request):
+    return HttpResponse("Olá mundo")
