1c91b0bc657c75f81e8afc3f7bd4564a82929ee6
Nath-veloz <nathansluz24@gmail.com>
2023-05-16 11:15:51 -0300

    migrando polls

--- polls/urls.py
+++ polls/urls.py
@@ -1,15 +1,7 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path('sobre/', views.sobre, name='sobre')
-]
-
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path("", views.index, name="index")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,4 @@
 from django.http import HttpResponse
 
-# Create your views here.
-
-# Nova view, leva para algum rota
 def index(request):
-    return HttpResponse("Olá mundo")
-
-def sobre(request):
-    return HttpResponse("Esse é um app de enquete")
+    return HttpResponse("Hello, world. You're at the polls index.")
